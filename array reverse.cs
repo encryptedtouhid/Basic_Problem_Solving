3549452ed76d5fdc5b80a1003cf5cc4c7c7f2cfe
Khaled Md Tuhidul Hossain <me@tuhidulhossain.com>
2020-04-26 20:24:12 +0600

    Add files via upload

---
+++ array reverse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[8] { 1, 2, 3, 4, 5, 6, 7,8 };
+
+            makeReverse(arr);           
+
+        }
+
+        public static void makeReverse(int[] arr)
+        {
+
+            for (int i = (arr.Length - 1)/2; i >= 0; i--)
+            {
+                int last = arr[i];
+                int first = arr[arr.Length -1 - i];
+
+                arr[i] = first;
+                arr[(arr.Length - 1) - i] = last;
+            }
+        }
+
+    }
+}
